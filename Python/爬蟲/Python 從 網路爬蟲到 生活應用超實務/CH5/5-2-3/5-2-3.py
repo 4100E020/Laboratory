b0fa65be3905b5f0397d57a171c83a336ced094d
4100E020 <90737813+4100E020@users.noreply.github.com>
2022-01-25 21:28:24 +0800

    Add files via upload

---
+++ Python/爬蟲/Python 從 網路爬蟲到 生活應用超實務/CH5/5-2-3/5-2-3.py
@@ -0,0 +1,8 @@
+import requests
+import re
+
+url = "https://fchart.github.io/"
+response = requests.get(url)
+links = re.findall(r'href="https://.*?"', response.text)
+for link in links:
+    print(link)
